5f6155cd16299e588fdcdc66b7ced8d5d05b64ba
oliverwaefler@gmail.com <oliverwaefler@gmail.com>
2021-09-24 16:55:19 +0200

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,36 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <!-- component -->
+  <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+    <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+      <div class=" h-32 overflow-hidden" >
+        <img class="w-full" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Fernfachhochschule_Schweiz_Logo.svg/400px-Fernfachhochschule_Schweiz_Logo.svg.png" alt="" />
+
+      </div>
+      <div class="flex justify-center px-5  -mt-12">
+        <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+
+      </div>
+      <div class=" ">
+        <div class="text-center px-14">
+          <h2 class="text-gray-800 text-3xl font-bold">Arbeitsauftrag 1</h2>
+          <p class="text-gray-400 mt-2">@Christian.Scharr</p>
+          <p class="text-gray-400 mt-2">@Oliver.Wäfler</p>
+          <p class="mt-2 text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+        </div>
+        <hr class="mt-6" />
+        <div class="flex  bg-gray-50 ">
+          <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+            <p><span class="font-semibold">2.5 k </span> Followers</p>
+          </div>
+          <div class="border"></div>
+          <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+            <p> <span class="font-semibold">2.0 k </span> Following</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,7 +1,34 @@
 <template>
-  <h1>{{ msg }}</h1>
-  <button @click="count++">count is: {{ count }}</button>
-  <p>Edit <code>components/HelloWorld.vue</code> to test hot module replacement.</p>
+  <!-- component -->
+  <div class="h-screen bg-gray-200  dark:bg-gray-800   flex flex-wrap items-center  justify-center  ">
+    <div class="container lg:w-2/6 xl:w-2/7 sm:w-full md:w-2/3    bg-white  shadow-lg    transform   duration-200 easy-in-out">
+      <div class=" h-32 overflow-hidden" >
+        <img class="w-full" src="https://images.unsplash.com/photo-1605379399642-870262d3d051?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2000&q=80" alt="" />
+      </div>
+      <div class="flex justify-center px-5  -mt-12">
+        <img class="h-32 w-32 bg-white p-2 rounded-full   " src="https" alt="" />
+
+      </div>
+      <div class=" ">
+        <div class="text-center px-14">
+          <h2 class="text-gray-800 text-3xl font-bold">Arbeitsauftrag1</h2>
+          <p class="text-gray-400 mt-2">@Oliver Wäfler, Christian Scharr</p>
+          <p class="mt-2 text-gray-600">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, </p>
+        </div>
+        <hr class="mt-6" />
+        <div class="flex  bg-gray-50 ">
+          <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+            <p><span class="font-semibold">2.5 k </span> Followers</p>
+          </div>
+          <div class="border"></div>
+          <div class="text-center w-1/2 p-4 hover:bg-gray-100 cursor-pointer">
+            <p> <span class="font-semibold">2.0 k </span> Following</p>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
